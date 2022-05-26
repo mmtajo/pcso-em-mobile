aea34d350cf4b2c36275187fe716b5218015b1c5
emtajo <mtajo@pcso.gov.ph>
2022-05-26 13:57:39 +0800

    Initial Mobile App Project

---
+++ pcso-em-mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace pcso_em_mobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ pcso-em-mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace pcso_em_mobile;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
